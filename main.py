8f52e6c86e39bc7a281eded3d24578d269ca2245
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2022-05-05 20:27:37 +0545

    Translation mechanism success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from textblob import TextBlob
 import googletrans
 
 
@@ -7,27 +8,6 @@ root = Tk()
 root.title('Translator App')
 root.iconbitmap('static/translator_icon.ico')
 root.geometry('915x430')
-
-
-def entryComboClick(event):
-    pass
-
-def resultComboClick(event):
-    pass
-def translationOperation():
-    pass
-
-# Text Box i.e 'Text-to-Translate' input field
-entryText = Text(root, height = 10, width = 45)
-entryText.grid(row = 0, column = 0, padx = 10, pady = 20)
-
-# Translate Button
-translateButton = Button (root, text = "Translate", font=("Helvetica", 16), command = translationOperation)
-translateButton.grid(row = 0, column = 1, padx = 20, pady = 10)
-
-# Text Box i.e 'Translated text'
-resultText = Text(root, height =10, width = 45)
-resultText.grid(row = 0, column = 2 , padx = 10, pady = 20)
 
 
 # Appending countries name in list through googletrans api
@@ -38,19 +18,55 @@ for key, value in languages.items():
     listOfLanguages.append(value)
 
 
-# Making combo box -> need to import ttk 
+
+
+def translationOperation():
+    resultText.delete(1.0, END)
+    
+    # Accessing text to be translated
+    textToBeTranslated = entryText.get(1.0, END)
+    text = TextBlob(textToBeTranslated)
+    
+    # Finding corresponding key from value in comboboxes
+    for key, value in languages.items():
+        if (value == entryCombo.get()):
+            from_key = key
+        if (value == resultCombo.get()):
+            to_key = key
+    
+    textTranslated = text.translate(from_lang=from_key, to=to_key)
+    resultText.insert(END, textTranslated)
+
+
+# Text Box i.e 'Text-to-Translate' input field
+entryText = Text(root, height=10, width=45)
+entryText.grid(row=0, column=0, padx=10, pady=20)
+
+# Translate Button
+translateButton = Button(root, text="Translate", font=(
+    "Helvetica", 16), command=translationOperation)
+translateButton.grid(row=0, column=1, padx=20, pady=10)
+
+# Text Box i.e 'Translated text'
+resultText = Text(root, height=10, width=45)
+resultText.grid(row=0, column=2, padx=10, pady=20)
+
+
+
+
+# Making combo box -> need to import ttk
 
 # Initial to be translated language mode
-entryCombo = ttk.Combobox(root, value = listOfLanguages)
+entryCombo = ttk.Combobox(root, value=listOfLanguages)
 entryCombo.current(21)
-entryCombo.bind("<<ComboboxSelected>>", entryComboClick)
-entryCombo.place(x = 60, y = 200)
+entryCombo.bind("<<ComboboxSelected>>")
+entryCombo.place(x=60, y=200)
 
 # Final Translated language mode
-resultCombo = ttk.Combobox(root, value = listOfLanguages)
+resultCombo = ttk.Combobox(root, value=listOfLanguages)
 resultCombo.current(21)
-resultCombo.bind("<<ComboboxSelected>>", resultComboClick)
-resultCombo.place(x = 700, y = 200)
+resultCombo.bind("<<ComboboxSelected>>")
+resultCombo.place(x=700, y=200)
 
 
 root.mainloop()
