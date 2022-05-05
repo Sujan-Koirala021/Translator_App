fd27fb28d0690b8ba2b697be84d80d4beb52de5b
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2022-05-05 20:47:35 +0545

    Simple workflow success

--- main.py
+++ main.py
@@ -26,15 +26,15 @@ def clear():
     # Setting comboboxes to default
     entryCombo.current(21)
     resultCombo.current(88)
-    
+
 
 def translationOperation():
     resultText.delete(1.0, END)
-    
+
     # Accessing text to be translated
     textToBeTranslated = entryText.get(1.0, END)
     text = TextBlob(textToBeTranslated)
-    
+
     try:
         # Finding corresponding key from value in comboboxes
         for key, value in languages.items():
@@ -42,15 +42,12 @@ def translationOperation():
                 from_key = key
             if (value == resultCombo.get()):
                 to_key = key
-        
+
         textTranslated = text.translate(from_lang=from_key, to=to_key)
         resultText.insert(END, textTranslated)
-    
+
     except Exception as e:
         messagebox.showerror("Translator", e)
-        
-    
-    
 
 
 # Text Box i.e 'Text-to-Translate' input field
@@ -65,27 +62,27 @@ translateButton.grid(row=0, column=1, padx=20, pady=10)
 
 # Clear Button
 clearButton = Button(root, text="Clear", font=(
-    "Helvetica", 14), command=clear)
-clearButton.place(x = 450,y = 200)
+    "Helvetica", 12), command=clear)
+clearButton.place(x=430, y=200)
 
 # Text Box i.e 'Translated text'
 resultText = Text(root, height=10, width=45)
 resultText.grid(row=0, column=2, padx=10, pady=20)
 
 
-
-
 # Making combo box -> need to import ttk
 
 # Initial to be translated language mode
 entryCombo = ttk.Combobox(root, value=listOfLanguages)
-entryCombo.current(21)          # Setting English Language as default (refering the dictionary)
+# Setting English Language as default (refering the dictionary)
+entryCombo.current(21)
 entryCombo.bind("<<ComboboxSelected>>")
 entryCombo.place(x=60, y=200)
 
 # Final Translated language mode
 resultCombo = ttk.Combobox(root, value=listOfLanguages)
-resultCombo.current(88)         # Setting Spanish language as default (refering the dictionary) 
+# Setting Spanish language as default (refering the dictionary)
+resultCombo.current(88)
 resultCombo.bind("<<ComboboxSelected>>")
 resultCombo.place(x=700, y=200)
 
